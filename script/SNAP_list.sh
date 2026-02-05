da1e66de7a2ec31827ae1a2da53cea735df3d9ca
Riccardo Speretta <riccardo.speretta@studenti.unitn.it>
2026-02-05 21:20:18 +0100

    complete setup

--- script/SNAP_list.sh
+++ script/SNAP_list.sh
@@ -4,8 +4,8 @@
 #   1 = assumed undirected, for each a->b edge, also b->a exists
 #   0 = assumed directed OR undirected but without the b->a edge
 declare -A SNAP_GRAPHS=(
-    ["com-dblp.ungraph"]=1
-    ["as-skitter"]=1
-    ["com-orkut.ungraph"]=1
-    ["com-friendster.ungraph"]=1
+    ["com-dblp.ungraph"]=0
+    ["as-skitter"]=0
+    ["com-orkut.ungraph"]=0
+    ["com-friendster.ungraph"]=0
 )
